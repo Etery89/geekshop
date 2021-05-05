2a6eb5c154e8ab2f0e725d69fa38f7503bf0cc94
Etery89 <shadowx110@yandex.ru>
2021-05-05 23:30:37 +0300

    added controller for login

--- authapp/views.py
+++ authapp/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
+from authapp.forms import UserLoginForm
 # Create your views here.
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user_name = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=user_name, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
     context = {
         'title': 'GeekShop - Авторизация',
+        'form': form,
     }
     return render(request, 'authapp/login.html', context)
 
