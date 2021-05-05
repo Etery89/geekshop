ea98bde309f27ecc27c04e2296185f70946d1287
Etery89 <shadowx110@yandex.ru>
2021-05-05 18:18:21 +0300

    added templates for login and register

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login(request):
+    context = {
+        'title': 'GeekShop - Авторизация',
+    }
+    return render(request, 'authapp/login.html', context)
+
