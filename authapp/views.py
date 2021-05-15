f92120129dd1c4d33b0f924e16a38f31e1d33b5e
Etery89 <shadowx110@yandex.ru>
2021-05-15 22:34:24 +0300

    added profile templates and profile controller in authapp

--- authapp/views.py
+++ authapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
 
-from authapp.forms import UserLoginForm, UserRegisterForm
+from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 # Create your views here.
 
 
@@ -39,6 +39,21 @@ def register(request):
         'form': form
     }
     return render(request, 'authapp/register.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'title': 'GeekShop - Личный кабинет',
+        'form': form
+    }
+    return render(request, 'authapp/profile.html', context)
 
 
 def logout(request):
