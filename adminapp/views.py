f0d047732356a17b2281264710d9c6a4b6cc066b
Etery89 <shadowx110@yandex.ru>
2021-05-23 14:55:40 +0300

    added check button for the staff in superuser in mainapp/base.html and added decorator for checking permission

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 
 from authapp.models import User
 from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
@@ -7,10 +8,12 @@ from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 # Create your views here.
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def index(request):
     return render(request, 'adminapp/admin.html')
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_read(request):
     context = {
         'users': User.objects.all()
@@ -18,6 +21,7 @@ def admin_users_read(request):
     return render(request, 'adminapp/admin-users-read.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
@@ -32,6 +36,7 @@ def admin_users_create(request):
     return render(request, 'adminapp/admin-users-create.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_update(request, user_id):
     selected_user = User.objects.get(id=user_id)
     if request.method == 'POST':
@@ -49,6 +54,7 @@ def admin_users_update(request, user_id):
     return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_remove(request, user_id):
     user = User.objects.get(id=user_id)
     user.is_active = False
@@ -56,6 +62,7 @@ def admin_users_remove(request, user_id):
     return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_recovery(request, user_id):
     user = User.objects.get(id=user_id)
     user.is_active = True
