cbf9549aeffd79d7365df009963c12a58c75b9cb
Etery89 <shadowx110@yandex.ru>
2021-07-05 19:23:08 +0300

    added functionality orders list and order create

---
+++ ordersapp/models.py
@@ -0,0 +1,51 @@
+from django.conf import settings
+from django.db import models
+
+from mainapp.models import Product
+
+
+class Order(models.Model):
+
+    FORMING = 'FM'
+    SENT_TO_PROCEED = 'STR'
+    PROCEEDED = 'PRD'
+    PAID = 'PD'
+    READY = 'RD'
+    CANCEL = 'CNC'
+    DELIVERED = 'DVD'
+
+    STATUSES = (
+        (FORMING, 'Формируется'),
+        (SENT_TO_PROCEED, 'Отправлен в обработку'),
+        (PROCEEDED, 'Обработан'),
+        (PAID, 'Оплачен'),
+        (READY, 'Готов к выдаче'),
+        (CANCEL, 'Отменён'),
+        (DELIVERED, 'Выдан')
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='создан')
+    updated = models.DateTimeField(auto_now=True, verbose_name='обновлён')
+    is_active = models.BooleanField(default=True)
+
+    status = models.CharField(choices=STATUSES, default=FORMING, verbose_name='статус', max_length=3)
+
+    def get_total_quantity(self):
+        items = self.orderitems.select_related()
+        total_quantity = sum(list(map(lambda x: x.quantity, items)))
+        return total_quantity
+
+    def get_total_cost(self):
+        items = self.orderitems.select_related()
+        total_cost = sum(list(map(lambda x: x.get_product_cost(), items)))
+        return total_cost
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orderitems')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='продукт')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='количество')
+
+    def get_product_cost(self):
+        return self.product.price * self.quantity
---
+++ ordersapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from ordersapp.models import Order, OrderItem
+from ordersapp.forms import OrderItemForm
+from django.urls import reverse_lazy
+from django.forms import inlineformset_factory
+
+
+class OrderList(ListView):
+    model = Order
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+
+class OrderCreate(CreateView):
+    model = Order
+    success_url = reverse_lazy('order:list')
+    fields = []
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST)
+        else:
+            formset = OrderFormSet()
+        data['orderitems'] = formset
+        return data
+
+
+class OrderUpdate(UpdateView):
+    pass
+
+
+class OrderDelete(DeleteView):
+    pass
+
+
+class OrderRead(DetailView):
+    pass
+
+
+def forming_complete(request, pk):
+    return render(request)
