3b65b29e54676b44dc874497d163a0a22cf4b374
Etery89 <shadowx110@yandex.ru>
2021-07-05 23:38:59 +0300

    completed homework to lesson 3

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -41,6 +41,13 @@ class Order(models.Model):
         total_cost = sum(list(map(lambda x: x.get_product_cost(), items)))
         return total_cost
 
+    def delete(self, using=None, keep_parents=False):
+        for item in self.orderitems.select_related():
+            item.product.quantity += item.quantity
+            item.product.save()
+        self.is_active = False
+        self.save()
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orderitems')
